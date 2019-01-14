14a0bad60ce67523582e12cf68348bd6bd3b23dc
Will Fenton <wfenton@ualberta.ca>
2019-01-14 00:15:48 -0700

    Fixed backend

--- movieApp.js
+++ movieApp.js
@@ -1,48 +1,35 @@
 $(document).ready(function (){
 
-	function get_movie_dictionary(query){
-		dictionary = {};
+	searchMovie = async function(dictionary) {
 
-		searchMovie(dictionary, query).then(function() {
-			getDetails(dictionary);
-		}).then(function () {
-			getCredits(dictionary);
-		}).then(function () {
-			getRelease(dictionary);
-		}).then(function () {
-			getKeywords(dictionary);
-		});
-
-		return dictionary;
-	}
-
-	function searchMovie(dictionary, query){
-		var deferred = $.Deferred();
 		var settings = {
 			"async": true,
 			"crossDomain": true,
-			"url": "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query="+query+"&language=en-US&api_key=07a7c9c75d334aae16ed5f47c759aa7e",
+			"url": "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query="+dictionary["searchTerm"]+"&language=en-US&api_key=07a7c9c75d334aae16ed5f47c759aa7e",
 			"method": "GET",
 			"headers": {},
 			"data": "{}"
 		};
 
-		$.ajax(settings).done(function (response) {
-
-			dictionary["id"]=response.results[0].id;
+		await $.ajax(settings).done(function (response) {
+			dictionary["id"]=String(response.results[0].id);
 			dictionary["title"] = response.results[0].title;
 			dictionary["poster_url"] = "http://image.tmdb.org/t/p/w500/" + response.results[0].poster_path;
 			dictionary["language"] = response.results[0].original_language;
 			dictionary["rating"] = response.results[0].vote_average;
-			dictionary["release_date"] = response.results[0].release_date;
+
+			var options = {year: 'numeric', month: 'long', day: 'numeric' };
+			var dateObject = new Date(Date.parse(response.results[0].release_date));
+			dictionary["release_date"] = dateObject.toLocaleDateString("en-US", options);
+
 			dictionary["summary"] = response.results[0].overview;
-			//return deferred.resolve();
 		});
-		return deferred.resolve();
+
+		getDetails(dictionary);
 	}
 
-	function getKeywords(dictionary){
-		var deferred = $.Deferred();
+	getKeywords = async function(dictionary){
+
 		var settings = {
 			"async": true,
 			"crossDomain": true,
@@ -50,20 +37,19 @@ $(document).ready(function (){
 			"method": "GET",
 			"headers": {},
 			"data": "{}"
-		}
+		};
 
-		$.ajax(settings).done(function (response) {
+		await $.ajax(settings).done(function (response) {
 
 			dictionary["tag"] = [];
-			for (i = 0; i < response.keywords.length; i++) {
+			for (var i = 0; i < response.keywords.length; i++) {
 				dictionary["tag"].push(response.keywords[i].name);
 			}
 		});
-		return deferred.resolve();
 	}
 
-	function getRelease(dictionary) {
-		var deferred = $.Deferred();
+	getRelease = async function(dictionary) {
+
 		var settings = {
 			"async": true,
 			"crossDomain": true,
@@ -71,57 +57,76 @@ $(document).ready(function (){
 			"method": "GET",
 			"headers": {},
 			"data": "{}"
-		}
+		};
 
-		$.ajax(settings).done(function (response) {
-			for (i = 0; i < response.results.length; i++) {
+		await $.ajax(settings).done(function (response) {
+			for (var i = 0; i < response.results.length; i++) {
 				if (response.results[i].iso_3166_1 === "US") {
 					dictionary["mpaa_rating"] = response.results[i].release_dates[0].certification;
 				}
 
 			}
 		});
-		return deferred.resolve();
+
+		getKeywords(dictionary);
 	}
 
-	function getDetails(dictionary){
-		var deferred = $.Deferred();
-		var settings = {
+	getDetails = async function(dictionary) {
 
+		var settings = {
 			"async": true,
 			"crossDomain": true,
-			"url": "https://api.themoviedb.org/3/movie/"+dictionary['id']+"?language=en-US&api_key=07a7c9c75d334aae16ed5f47c759aa7e",
+			"url": "https://api.themoviedb.org/3/movie/"+dictionary["id"]+"?api_key=07a7c9c75d334aae16ed5f47c759aa7e&language=en-US",
 			"method": "GET",
 			"headers": {},
 			"data": "{}"
 		};
 
-		var i;
+		await $.ajax(settings).done(function (response) {
 
-		$.ajax(settings).done(function (response) {
-
-			dictionary["genre"] = [];
-			for (i = 0; i < response.genres.length; i++) {
-				dictionary["genre"].push(response.genres[i].name);
+			var num_genres = response.genres.length;
+			dictionary["genres"] = "";
+			for (var i = 0; i < num_genres - 1; i++){
+				dictionary["genres"] = dictionary["genres"].concat(response.genres[i].name);
+				dictionary["genres"] = dictionary["genres"].concat(", ");
 			}
-			dictionary['budget'] = response.budget;
+			dictionary["genres"] = dictionary["genres"].concat(response.genres[num_genres - 1].name);
+
+			var formatter = new Intl.NumberFormat("en-US", {
+				style: "currency",
+				currency: "USD",
+				minimumFractionDigits: 0,
+			  });
+
+			dictionary['budget'] = formatter.format(response.budget);
 			dictionary['imdb_id'] = response.imdb_id;
 
-			dictionary["production_companies"] = [];
-			for (i = 0; i < response.production_companies.length; i++) {
-				dictionary["production_companies"].push(response.production_companies[i].name);
+			var num_companies = response.production_companies.length;
+			dictionary["productionCompanies"] = "";
+			for (var i = 0; i < num_companies - 1; i++){
+				dictionary["productionCompanies"] = dictionary["productionCompanies"].concat(response.production_companies[i].name);
+				dictionary["productionCompanies"] = dictionary["productionCompanies"].concat(", ");
 			}
+			dictionary["productionCompanies"] = dictionary["productionCompanies"].concat(response.production_companies[num_companies - 1].name);
 
 			dictionary['homepage'] = response.homepage;
-			dictionary['box_office'] = response.revenue;
-			dictionary['runtime'] = response.runtime;
-			return deferred.resolve();
+			dictionary['box_office'] = formatter.format(response.revenue);
+
+			if (Math.floor(response.runtime / 60) == 0) {
+				dictionary["runtime"] = String(response.runtime) + " minutes";
+			} else if (Math.floor(response.runtime / 60) == 1) {
+				dictionary["runtime"] = String(Math.floor(response.runtime / 60)) + " hour, " + String(response.runtime % 60) + " minutes";
+			} else {
+				dictionary["runtime"] = String(Math.floor(response.runtime / 60)) + " hours, " + String(response.runtime % 60) + " minutes";
+			}
 		});
+
+		getCredits(dictionary);
 
 	}
 
-	function getCredits(dictionary){
-		var deferred = $.Deferred();
+	getCredits = async function(dictionary){
+
 		var settings = {
 			"async": true,
 			"crossDomain": true,
@@ -130,166 +135,229 @@ $(document).ready(function (){
 			"headers": {},
 			"data": "{}"
 		};
-		var i, max;
 
-		$.ajax(settings).done(function (response) {
+		await $.ajax(settings).done(function (response) {
 			if (response.cast.length < 5){
-				max = response.cast.length
+				var max = response.cast.length;
 			} else {
-				max = 5
+				var max = 5;
 			}
 
-			dictionary['actor'] = [];
-			for (i=0; i < max; i++){
-				dictionary['actor'].push(response.cast[i].name);
+			dictionary["actors"] = "";
+			for (var i = 0; i < max - 1; i++){
+				dictionary["actors"] = dictionary["actors"].concat(response.cast[i].name);
+				dictionary["actors"] = dictionary["actors"].concat(", ");
 			}
+			dictionary["actors"] = dictionary["actors"].concat(response.cast[max].name);
 
-			for(i=0; i < response.crew.length; i++) {
+			directors = [];
+			for(var i = 0; i < response.crew.length; i++) {
 				if (response.crew[i].job === "Director") {
-					dictionary['director'] = response.crew[i].name;
+					directors.push(response.crew[i].name);
 				}
 			}
-			return deferred.resolve();
+			dictionary["directors"] = directors.join(", ");
 		});
+
+		getRelease(dictionary);
 
 	}
 
-		function addMovies(search_terms) {
+		addMovies = async function(search_terms) {
 
 			var terms = search_terms.split(',');
 
-			console.log(search_terms);
-			console.log(terms);
-
 			var movies = new Array();
-			
+
 			for (var i = 0; i < terms.length; i++) {
-				movies.push(get_movie_dictionary(terms[i]));
+				var movie = {};
+				movie["searchTerm"] = terms[i];
+				movies.push(movie);
+			}
+			
+			for (var i = 0; i < movies.length; i++) {
+				await searchMovie(movies[i]);
 			}					
 
+			console.log(movies);
+
 			setTimeout(function(){
-
-				console.log(movies);
-
-				//Create a HTML Table element.
-				var table = document.createElement("TABLE");
-				table.className = "table table-striped";
-			
-				var numFields = 5;
-			
-				//Add the header row.
-
-				var posterWidth = window.innerWidth / (7) - 10;
-
-				var row = table.insertRow(-1);
-
-				var headerCell = document.createElement("TH");
-				headerCell.innerHTML = ""
-				headerCell.padding="0";
-				headerCell.style.width=posterWidth;
-
-				row.appendChild(headerCell)
-
-				for (var i = 0; i < movies.length; i++) {
-					var headerCell = document.createElement("TH");
-					headerCell.innerHTML = "";
-					headerCell.padding="0";
-					headerCell.style.width=posterWidth;
-
-					var img = document.createElement('img');
-					img.src = movies[i]["poster_url"];
-					img.padding="0";
-					img.width= posterWidth;
-					img.height=posterWidth*1.5;
-					
-					headerCell.appendChild(img);
-
-					row.appendChild(headerCell);
-				}
-
-				//Add the data rows.
-				row = table.insertRow(-1);
-				var cell = row.insertCell(-1);
-				cell.innerHTML = "Title";
-
-				for (var i = 0; i < movies.length; i++) {
-					var cell = row.insertCell(-1);
-					cell.innerHTML = movies[i]["title"];
-				}
-
-				row = table.insertRow(-1);
-				var cell = row.insertCell(-1);
-				cell.innerHTML = "Release Date";
-
-				for (var i = 0; i < movies.length; i++) {
-					var cell = row.insertCell(-1);
-					cell.innerHTML = movies[i]["release_date"];
-				}
-				
-
-				row = table.insertRow(-1);
-				var cell = row.insertCell(-1);
-				cell.innerHTML = "User Rating";
-
-				for (var i = 0; i < movies.length; i++) {
-					var cell = row.insertCell(-1);
-					cell.innerHTML = movies[i]["rating"];
-				}
-
-				row = table.insertRow(-1);
-				var cell = row.insertCell(-1);
-				cell.innerHTML = "Summary";
-
-				for (var i = 0; i < movies.length; i++) {
-					var cell = row.insertCell(-1);
-					cell.innerHTML = movies[i]["summary"];
-				}
-
-				var dvTable = document.getElementById("dvTable");
-				dvTable.innerHTML = "";
-				dvTable.appendChild(table);
-
-			}, 1000);
-
+				drawTable(movies);
+			}, 3000);
 		}
 
+		function drawTable(movies) {
 
-		
+			//Create a HTML Table element.
+			var table = document.createElement("TABLE");
+			table.className = "table table-striped";
+			
+			//Add the header row.
+			var posterWidth = String(Number(window.innerWidth / (8))) + "px";
+			console.log(posterWidth);
 
-	$("#tablebtn").click(function() {
-		console.log("2");
-	    $([document.documentElement, document.body]).animate({
-	        scrollTop: $("#dvTable").offset().top
-	    }, 1000);
-	});
-	
-	$('#div1').css({ opacity: 0 });
-	$('#div2').css({ opacity: 0 });
-	$('#div3').css({ opacity: 0 });
-	$('#div4').css({ opacity: 0 });
-	$('#div5').css({ opacity: 0 });
+			var row = table.insertRow(-1);
+
+			var headerCell = document.createElement("TH");
+			headerCell.innerHTML = ""
+			// headerCell.padding = "0";
+			// headerCell.style.width = posterWidth;
+
+			row.appendChild(headerCell)
+
+			for (var i = 0; i < movies.length; i++) {
+				var headerCell = document.createElement("TH");
+				headerCell.innerHTML = "";
+				// headerCell.padding = "0";
+				// headerCell.style.width = posterWidth;
+
+				var img = document.createElement('img');
+				img.src = movies[i]["poster_url"];
+				img.padding = "0";
+				img.style.width = posterWidth;
+				// img.height =posterWidth*1.5;
+				
+				headerCell.appendChild(img);
+
+				row.appendChild(headerCell);
+			}
+
+			//Add the data rows.
+
+			//Title
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Title";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["title"];
+			}
+
+			//Director
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Director";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["directors"];
+			}
+			
+			//Actors
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Lead actors";
+
+			for (var i = 0; i < movies.length; i++) {
+
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["actors"];
+			}
+
+			//Runtime
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Runtime";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["runtime"];
+			}
+
+			//Genre
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Genres";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["genres"]
+			}
+
+			//Production Companies
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Production companies";
+
+			for (var i = 0; i < movies.length; i++) {
+
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["productionCompanies"];
+			}
+
+			//Release date
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Release date";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["release_date"];
+			}
+
+			//MPAA Rating
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "MPAA Rating";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["mpaa_rating"];
+			}
+
+			//Budget
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Budget (USD)";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["budget"];
+			}
+
+			//Box office
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Box office (USD)";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["box_office"];
+			}
+			
+			//User rating
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "User rating";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["rating"];
+			}
+
+			//Summary
+			row = table.insertRow(-1);
+			var cell = row.insertCell(-1);
+			cell.innerHTML = "Summary";
+
+			for (var i = 0; i < movies.length; i++) {
+				var cell = row.insertCell(-1);
+				cell.innerHTML = movies[i]["summary"];
+			}
+
+
+
+			var dvTable = document.getElementById("dvTable");
+			dvTable.innerHTML = "";
+			dvTable.appendChild(table);
+		}
 
 	// If search was clicked or return key was hit
 	$('.icon').click(function(){
-        /*if ($(this).data('count')) { // was clicked
-            $(this).data('count', $(this).data('count') + 1); // add one
-        } else { // no clicks
-            $(this).data('count', 1); // initialize the count
-        }
-        if ( $(this).data('count')==1){
-        	$('#div1').css({ opacity: 1 });
-        } else if ( $(this).data('count')==2){
-        	$('#div2').css({ opacity: 1 });
-        } else if ( $(this).data('count')==3){
-        	$('#div3').css({ opacity: 1 });
-        } else if ( $(this).data('count')==4){
-        	$('#div4').css({ opacity: 1 });
-        } else {
-        	$('#div5').css({ opacity: 1 });
-		}*/
+
 		var value = $("#search-input").val();
 		console.log(value);
-
 		$("#search-input").val("");
 
 		addMovies(value);
@@ -305,17 +373,6 @@ $(document).ready(function (){
         if (e.which == 13) {
             $('.icon').trigger("click");
         }
-	});
-	
-	$('.has-clear input[type="text"]').on('input propertychange', function() {
-		var $this = $(this);
-		var visible = Boolean($this.val());
-		$this.siblings('.form-control-clear').toggleClass('hidden', !visible);
-	  }).trigger('propertychange');
-	  
-	  $('.form-control-clear').click(function() {
-		$(this).siblings('input[type="text"]').val('')
-		  .trigger('propertychange').focus();
 	});
 	
 });
